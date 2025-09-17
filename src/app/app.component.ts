33bfc21e62da784504276bf0adb9e3176dccd818
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-17 09:30:53 +0500

    7. Property Binding Repetition

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,11 +3,12 @@ import { HeaderComponent } from './header/header.component';
 import { ServerStatusComponent } from "./dashboard/server-status/server-status.component";
 import { StrafficComponent } from "./dashboard/straffic/straffic.component";
 import { TicketsComponent } from "./dashboard/tickets/tickets.component";
+import { DashboardItemComponent } from "./dashboard/dashboard-item/dashboard-item.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [HeaderComponent, ServerStatusComponent, StrafficComponent, TicketsComponent],
+  imports: [HeaderComponent, ServerStatusComponent, StrafficComponent, TicketsComponent, DashboardItemComponent],
   templateUrl: './app.component.html',
 })
 export class AppComponent {
