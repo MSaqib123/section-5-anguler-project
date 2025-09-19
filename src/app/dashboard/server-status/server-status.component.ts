698cca90a22c63eb12f57c5e74d365c18f1c184c
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-19 21:24:56 +0500

    29. Implementing Lifecycle Interfaces

--- src/app/dashboard/server-status/server-status.component.ts
+++ src/app/dashboard/server-status/server-status.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-server-status',
@@ -6,7 +6,12 @@ import { Component } from '@angular/core';
   templateUrl: './server-status.component.html',
   styleUrl: './server-status.component.css'
 })
-export class ServerStatusComponent {
+//=========== interface or structure of the component =============
+// to avoid the ngInit error we have to implement the OnInit interface
+// becuase Angular does not know that we are using the ngOnInit lifecycle hook
+// so now when we type  small case ngOnInit to ngoninit it will give error
+// so to avoid this error we have to implement the OnInit interface and to implement correct structure
+export class ServerStatusComponent implements OnInit {
   // typo type or enum type
   currentStatus:'online'|'offline'|'unknown' = 'online';
 
