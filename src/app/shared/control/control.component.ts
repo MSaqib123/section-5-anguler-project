26bad9330cef8458408bcf2a459ab480ff759b6e
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-19 12:10:47 +0500

    17. Understanding & Configuring View Encapsulation

--- src/app/shared/control/control.component.ts
+++ src/app/shared/control/control.component.ts
@@ -1,10 +1,12 @@
-import { Component, input } from '@angular/core';
+import { Component, input, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-control',
   imports: [],
   templateUrl: './control.component.html',
-  styleUrl: './control.component.css'
+  styleUrl: './control.component.css',
+  //======= remove scoped css global ========
+  encapsulation: ViewEncapsulation.None, // ViewEncapsulation.None
 })
 export class ControlComponent {
   label = input<string>();
