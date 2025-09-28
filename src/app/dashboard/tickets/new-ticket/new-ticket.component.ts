7ab894916e413e719fed0b4a8e2e7aa4697066c8
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-29 00:32:40 +0500

    34. Working with Template Variables

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,16 +1,22 @@
 import { Component, input } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
 
 @Component({
   selector: 'app-new-ticket',
-  imports: [ButtonComponent, ControlComponent],
+  imports: [ButtonComponent, ControlComponent,FormsModule],
   templateUrl: './new-ticket.component.html',
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
-  onSubmit() {  
-    console.log('Form submitted');
+  onSubmit(titleInput:HTMLInputElement,textArea:HTMLTextAreaElement) {  
+    console.dir(titleInput);
+    console.dir(textArea);
+
+    console.log('Form submitted', titleInput.value, textArea.value);
   } 
-  
+
+
+
 }
