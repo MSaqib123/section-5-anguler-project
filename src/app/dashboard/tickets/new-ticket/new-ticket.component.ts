5b5faf34501dbf2fcfc3c0de7ab6b4eefad0d1cb
M Boota <48630668+MSaqib123@users.noreply.github.com>
2025-09-29 01:23:08 +0500

    37. Getting Access to Template Elements via ViewChild

--- src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
+++ src/app/dashboard/tickets/new-ticket/new-ticket.component.ts
@@ -1,4 +1,4 @@
-import { Component, input } from '@angular/core';
+import { Component, ElementRef, input, ViewChild } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { ButtonComponent } from "../../../shared/button/button.component";
 import { ControlComponent } from "../../../shared/control/control.component";
@@ -10,16 +10,25 @@ import { ControlComponent } from "../../../shared/control/control.component";
   styleUrl: './new-ticket.component.css'
 })
 export class NewTicketComponent {
+  //======= Direct Compoennt of button =========
+  // @ViewChild(ButtonComponent) submitButton: ButtonComponent;
+
+  // ======= Direct Template Variables =========
+  @ViewChild('form') form?: ElementRef<HTMLFormElement>;
+
 
   // onSubmit(titleInput:HTMLInputElement,textArea:HTMLTextAreaElement) {  
-  onSubmit(titleInput:string,textArea:string,form:HTMLFormElement) {  
+  // onSubmit(titleInput:string,textArea:string,form:HTMLFormElement) {  
+  onSubmit(titleInput:string,textArea:string) {  
     // console.log('Form submitted', titleInput.value, textArea.value);
     // console.dir(titleInput);
     // console.dir(textArea);
 
     console.log(titleInput);
     console.log(textArea);
-    form.reset();
+    // form.reset();
+
+    this.form?.nativeElement.reset();
 
   } 
 
